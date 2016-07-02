a26a63107c44948cc00eb0a880364f8252da67d5
Altaf-Mahdi <altaf.mahdi@gmail.com>
2016-07-02 03:35:00 +0100

    oneplus2: properly add package rule for com.qualcomm.location.apk
    missed from 88e5a6093697b96fca0dc22557d49b6be324b8b6
    
    Change-Id: I28d00412357ce120fc2fef8f0c45f36dd3a7475a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -97,6 +97,9 @@ PRODUCT_PACKAGES += \\
     colorservice \\
     pp_calib_data_jd35695_1080p_cmd_mode_dsi_panel.xml
 
+PRODUCT_PACKAGES += \\
+    com.qualcomm.location
+
 \$(call inherit-product, vendor/$VENDOR/$DEVICE/$DEVICE-vendor-blobs.mk)
 EOF
 
@@ -454,6 +457,17 @@ LOCAL_MODULE       := pp_calib_data_jd35695_1080p_cmd_mode_dsi_panel.xml
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := proprietary/etc/pp_calib_data_jd35695_1080p_cmd_mode_dsi_panel.xml
+include \$(BUILD_PREBUILT)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := com.qualcomm.location
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_CERTIFICATE := platform
 include \$(BUILD_PREBUILT)
 
 \$(shell mkdir -p \$(PRODUCT_OUT)/system/vendor/lib/egl && pushd \$(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
