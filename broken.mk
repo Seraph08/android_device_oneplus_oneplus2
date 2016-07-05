dab5a0b33dfbf58533e3d033c94b8dbc516c9a15
David Vandenburgh <davidvandenburgh@linux.com>
2016-07-05 14:24:14 -0400

    Update and rename broken_oneplus2.mk to broken.mk

--- broken.mk
+++ broken.mk
@@ -22,22 +22,5 @@ $(call inherit-product, device/oneplus/oneplus2/device.mk)
 # Inherit some common Broken shit.
 $(call inherit-product, vendor/broken/config/common_full_phone.mk)
 
-PRODUCT_NAME := broken_oneplus2
-PRODUCT_DEVICE := oneplus2
-PRODUCT_MANUFACTURER := OnePlus
-PRODUCT_BRAND := OnePlus
 
-PRODUCT_GMS_CLIENTID_BASE := android-oneplus
-
-TARGET_VENDOR_PRODUCT_NAME := OnePlus2
-TARGET_VENDOR_DEVICE_NAME := OnePlus2
-PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=OnePlus2 PRODUCT_NAME=OnePlus2
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=OnePlus/OnePlus2/OnePlus2:6.0.1/MMB29M/1447840820:user/release-keys \
-    PRIVATE_BUILD_DESC="OnePlus2-user 6.0.1 MMB29M 31 dev-keys"
-
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model
-
-TARGET_VENDOR := oneplus
 
